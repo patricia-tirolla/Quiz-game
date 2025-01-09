c5bb106b5a4126d7b16c9ab24322e70024dff713
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2025-01-09 17:00:44 +0100

    Fix test for increaseScoreCondition

--- spec/quizSpec.mjs
+++ spec/quizSpec.mjs
@@ -2,15 +2,23 @@
 import { quizLogic, quizStructure } from "../src/quizLogic.js";
 import { api } from "../src/fetchApi.js"
 describe("Quiz Logic", () => {
-    // beforeEach(async function() {
-    //     await api.getData();
-    // })
+    beforeEach(() => {
+        api.json = {
+            results: [
+                {
+                    question: "Which band released the album 'The Fat of the Land'?",
+                    correct_answer: "The Prodigy",
+                    incorrect_answers: ["The Chemical Brothers", "Fatboy Slim", "Orbital"]
+                },
+        ]}
+    })
     it("should increase the score when the winCondition is met", () => {
         expect(quizStructure.score).toBe(0);
         quizLogic.increaseScore();
         expect(quizStructure.score).toBe(1);
     })
-    // it("should return 'true' if the answer matches the correct answer", () => {
-    //     expect(quizLogic.winCondition("The Prodigy", 1)).toBe(true);
-    // })
+    it("should return 'true' if the answer matches the correct answer", () => {
+        console.log(api.json)
+        expect(quizLogic.increaseScoreCondition("The Prodigy", 0)).toBe(true);
+    })
 })
