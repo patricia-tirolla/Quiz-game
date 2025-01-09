a9c6b1f700c36197ad6c0256422e4198520f9a9e
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2025-01-09 16:51:15 +0100

    Add test for increaseScore function

--- spec/quizSpec.mjs
+++ spec/quizSpec.mjs
@@ -1,7 +1,16 @@
-import { quizLogic, quizStructure } from "../src/quizLogic";
 
-describe("Quiz Structure", () => {
-    isWebTarget("", () => {
-        
+import { quizLogic, quizStructure } from "../src/quizLogic.js";
+import { api } from "../src/fetchApi.js"
+describe("Quiz Logic", () => {
+    // beforeEach(async function() {
+    //     await api.getData();
+    // })
+    it("should increase the score when the winCondition is met", () => {
+        expect(quizStructure.score).toBe(0);
+        quizLogic.increaseScore();
+        expect(quizStructure.score).toBe(1);
     })
+    // it("should return 'true' if the answer matches the correct answer", () => {
+    //     expect(quizLogic.winCondition("The Prodigy", 1)).toBe(true);
+    // })
 })
