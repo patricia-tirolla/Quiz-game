4da13f10244e098d3f9ca2aa0502c66d391bcac4
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2025-01-13 18:48:11 +0100

    Fix more CSS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { api } from "./fetchApi";
 import { gameFlow } from "./quizUI";
 import style from "./styles/style.css";
-import jsStyles from "./styles/jsStyles.css"
+import jsStyles from "./styles/jsStyles.css";
 
-console.log(await api.getData());
+await api.getData();
 
 
 
